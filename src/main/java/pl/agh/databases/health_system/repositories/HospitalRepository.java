4900d697d9585cdc0260daee068a7314c84224e1
Seweryn Tasior <seweryn.tasior@op.pl>
2025-05-20 01:56:45 +0200

    deleting misplaced outdated backend files

--- src/main/java/pl/agh/databases/health_system/repositories/HospitalRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.agh.databases.health_system.repositories;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import pl.agh.databases.health_system.models.Hospital;
-
-public interface HospitalRepository extends Neo4jRepository<Hospital, Long> {
-
-}
